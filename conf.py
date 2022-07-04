cd616844eb30ee9e046e0cd914e663eb913c4b17
keyestudio <keyestudio925@gmail.com>
2022-07-04 12:33:22 +0800

    main

--- conf.py
+++ conf.py
@@ -17,9 +17,10 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'coding box'
-copyright = 'kidsbits 2022'
+project = 'coding robot'
+copyright = '2022, kidsbits'
 author = 'kidsbits'
+
 
 # -- General configuration ---------------------------------------------------
 
@@ -27,6 +28,7 @@ author = 'kidsbits'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ["myst_parser"]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
--- index.rst
+++ index.rst
@@ -1,13 +1,9 @@
 
 .. toctree::
    :maxdepth: 5
-   :caption: KD0003 Kidsbits coding robot
+   :caption: KD0003 Coding Robot for Arduino
 
-   KD0003.md
-
-
-
-
-
-
-
+   
+   KD0003.md  
+  
+ 
